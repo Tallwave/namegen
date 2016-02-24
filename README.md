e9076be879f2e89ee1b0e01a3c1dc542d5a32934
Scott Williams <scott@swilliams.me>
2016-02-23 22:33:59 -0700

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# namegen
+A very simple node server that returns randomly generated names using [faker.js](https://github.com/Marak/faker.js).
+
+# URLS
+`/names`
+
+Returns a JSON array of randomly generated names like this:
+
+```json
+{
+    "id": "cc050911-ade7-41d8-8b0c-26906e5858a3",
+    "name": "Nikita Mohr",
+    "email": "Eudora_Hessel@gmail.com",
+    "address": "4102 Maia Square",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/tjrus/128.jpg"
+  }
+```
+
+You can optionally add a querystring to set the number of names returned, by default it is 20. `/names?num=10`
+
+`/names/:some-id`
+
+Returns a single JSON object of a randomly generated name. Do note that this is really random, if you take an `id` from `/names`, it will almost certainly **not** be the same one returned here.
+
+# License
+MIT. See the LICENSE text file for details.
---
+++ index.js
@@ -0,0 +1,39 @@
+var restify = require('restify');
+var faker = require('faker');
+
+function randomName() {
+  var fakeName = {
+    id: faker.random.uuid(),
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    address: faker.address.streetAddress(),
+    image: faker.image.avatar()
+  };
+  return fakeName;
+}
+
+function handleNames(req, res, next) {
+  var numberOfNames = req.query.num || 20;
+  var fakeNames = [];
+  for (var i = 0; i < numberOfNames; i += 1) {
+    fakeNames.push(randomName());
+  }
+  res.json(fakeNames);
+  next();
+}
+
+function handleName(req, res, next) {
+  var name = randomName();
+  name.bio = faker.lorem.paragraph();
+  res.json(name);
+  next();
+}
+
+var server = restify.createServer();
+server.use(restify.queryParser());
+server.get('/names', handleNames);
+server.get('/names/:id', handleName);
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+})
